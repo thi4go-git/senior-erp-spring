7ac830869b34749632b9cc7a0abe5e0462320ed5
Thiago junior <thi4go19@gmail.com>
2024-07-17 21:28:57 -0300

    Subindo projeto / configurações básicas

---
+++ src/main/java/com/dynns/cloudtecnologia/senior/SeniorErpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.senior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorErpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorErpSpringApplication.class, args);
+	}
+
+}
