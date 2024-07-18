300d477e8599ad1c7a2ccd304984b795e904a889
Thiago junior <thi4go19@gmail.com>
2024-07-17 21:48:27 -0300

    Criando exception personalizada + Objeto para padronização de retorno de erros API

---
+++ src/main/java/com/dynns/cloudtecnologia/senior/exception/ApiErrors.java
@@ -0,0 +1,18 @@
+package com.dynns.cloudtecnologia.senior.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ApiErrors {
+    private List<String> erros;
+
+    public ApiErrors(String message) {
+        this.erros = Collections.singletonList(message);
+    }
+
+}
