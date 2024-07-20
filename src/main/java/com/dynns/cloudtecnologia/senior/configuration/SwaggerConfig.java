931914a63f326d96245ce2683972d903b4ead0eb
Thiago junior <thi4go19@gmail.com>
2024-07-20 18:01:05 -0300

    Informando Token JWT para o Swagger

--- src/main/java/com/dynns/cloudtecnologia/senior/configuration/SwaggerConfig.java
+++ src/main/java/com/dynns/cloudtecnologia/senior/configuration/SwaggerConfig.java
@@ -6,15 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.dynns.cloudtecnologia.senior.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .apiInfo(apiInfo());
+    }
 
     private Contact contact() {
         return new Contact("Thiago Junior", "https://www.linkedin.com/in/thiago-amorim-melo/", "thi4go19@gmail.com");
@@ -29,16 +47,27 @@ public class SwaggerConfig {
                 .build();
     }
 
-    @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("com.dynns.cloudtecnologia.senior.rest.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
 
 }
